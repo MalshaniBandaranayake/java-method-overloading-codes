722a4c45b84a6e01c778cc4bb05e16396d8cfa07
MalshaniBandaranayake <malshanibandaranayake91@gmail.com>
2023-04-25 10:07:15 +0530

    java method over loading codes

--- src/dog.java
+++ src/dog.java
@@ -1,16 +1,16 @@
 public class dog {
-    void dog(int q , int r) {
+    void cat(int q , int r) {
         System.out.println(q-r);
 
     }
-    void dog(String b ) {
+    void cat(String b ) {
         System.out.println("bark");
 
     }
 
     public static void main(String[] args) {
         dog d = new dog();
-        d.dog(10 , 5);
-        d.dog("bark");
+        d.cat(10 , 5);
+        d.cat("bark");
     }
 }
--- src/sum.java
+++ src/sum.java
@@ -1,16 +1,16 @@
 public class sum {
-    void sum(int a , long b) {
+    void cal(int a , long b) {
         System.out.println(a+b);
 
     }
-    void sum (int c , int d){
+    void cal (int c , int d){
         System.out.println(c+d);
 
     }
 
     public static void main(String[] args) {
         sum s = new sum();
-        s.sum(5 , 125L);
-        s.sum( 9 , 253);
+        s.cal(5 , 125L);
+        s.cal( 9 , 253);
     }
 }
